dbf659a6608474c5d4c13969ffd1ba0f42019b32
rphi8796 <jlkfop@gmail.com>
2018-11-27 12:11:33 -0700

    created the panel class

--- src/guitwo/view/GUITwoFrame.java
+++ src/guitwo/view/GUITwoFrame.java
@@ -1,6 +1,30 @@
 package guitwo.view;
 
-public class GUITwoFrame
-{
+import javax.swing.JFrame;
+import guitwo.controller.GUITwoController;
 
+public class GUITwoFrame extends JFrame
+{
+	private GUITwoController appController;
+	private GUITwoPanel appPanel;
+	
+	public GUITwoFrame(GUITwoController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new GUITwoPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setSize(800, 800);
+		this.setTitle("hmmmm");
+		this.setResizable(true);
+		this.setVisible(true);
+	}
+	
+	
 }
---
+++ src/guitwo/view/GUITwoPanel.java
@@ -0,0 +1,27 @@
+package guitwo.view;
+
+import javax.swing.*;
+import guitwo.controller.GUITwoController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUITwoPanel extends JPanel
+{
+	private GUITwoController appController;
+	private JLabel firstLabel;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+	
+	public GUITwoPanel(GUITwoController apController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+		firstLabel = new Jlabel("word in a line");
+		firstButton = new 
+	}
+	
+	
+}
