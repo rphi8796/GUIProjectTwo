8607e7e6a3bac68901adaac53c1f9bcf0bc540e5
rphi8796 <jlkfop@gmail.com>
2018-11-27 13:09:54 -0700

    Update GUITwoPanel.java

--- src/guitwo/view/GUITwoPanel.java
+++ src/guitwo/view/GUITwoPanel.java
@@ -28,6 +28,16 @@ public class GUITwoPanel extends JPanel
 		setupListeners();
 	}
 	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+		firstLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
+	}
+	
 	private void setupPanel()
 	{
 		this.setLayout(appLayout);
@@ -37,12 +47,21 @@ public class GUITwoPanel extends JPanel
 	
 	private void setupLayout()
 	{
-		
+		appLayout.putConstraint(SpringLayout.NORTH, firstLabel, 70, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstLabel, -90, SpringLayout.EAST,  this);
+		appLayout.putConstraint(SpringLayout.WEST, firstButton, 175, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, -135, SpringLayout.SOUTH, this);
 	}
 	
 	private void setupListeners()
 	{
-		firstButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent mouseClick){}});
+		firstButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeBackgroundColor();
+			}
+		});
 	}
 	
 	
